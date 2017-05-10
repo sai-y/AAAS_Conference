18b6da7f917d1b9d8c8c7a3cb421aeb3b9aca161
sai-y <sai@altschool.com>
2017-05-09 23:32:53 -0700

    Fitbit client

---
+++ fitbit_client_conference.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+    Personal health improvement example
+"""
+
+import fitbit
+import datetime
+import configparser
+
+# config is loaded from config file
+# alternatively you may store them as constants in your program
+CONFIG_FILE = '/home/pi/config.ini'
+config = configparser.ConfigParser()
+config.read(CONFIG_FILE)
+
+CONSUMER_KEY = config.get("APP", "CONSUMER_KEY")
+CONSUMER_SECRET = config.get("APP", "CONSUMER_SECRET")
+REFRESH_TOKEN = config.get("USER", "REFRESH_TOKEN")
+ACCESS_TOKEN = config.get("USER", "ACCESS_TOKEN")
+
+
+if __name__ == "__main__":
+    fbit_client = fitbit.Fitbit(CONSUMER_KEY,
+                                CONSUMER_SECRET,
+                                access_token=ACCESS_TOKEN,
+                                refresh_token=REFRESH_TOKEN)
+    now = datetime.datetime.now()
+    end_time = now.strftime("%H:%M")
+    response = fbit_client.intraday_time_series('activities/steps',
+                                                detail_level='15min',
+                                                start_time="00:00",
+                                                end_time=end_time)
+    print(response['activities-steps'][0]['value'])
+
+    response = fbit_client.activities_daily_goal()
+    print(response['goals']['steps'])
