12c81632865f0eb626e389d8aab7fe399986e530
sai-y <sai@altschool.com>
2017-05-11 15:59:03 -0700

    Fitbit client

--- fitbit_client_conference.py
+++ fitbit_client_conference.py
@@ -7,30 +7,108 @@ import fitbit
 import datetime
 import configparser
 
-# config is loaded from config file
-# alternatively you may store them as constants in your program
-CONFIG_FILE = '/home/pi/config.ini'
-config = configparser.ConfigParser()
-config.read(CONFIG_FILE)
+NUM_PLAYERS = 2
 
-CONSUMER_KEY = config.get("APP", "CONSUMER_KEY")
-CONSUMER_SECRET = config.get("APP", "CONSUMER_SECRET")
-REFRESH_TOKEN = config.get("USER", "REFRESH_TOKEN")
-ACCESS_TOKEN = config.get("USER", "ACCESS_TOKEN")
 
+class VisualAid(object):
+    """
+    A wrapper around the Fitbit API and different functions. 
+    """
+
+    def __init__(
+        self, player, consumer_key, consumer_secret,
+        access_token, refresh_token
+    ):
+        self.player = player
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        self.access_token = access_token
+        self.refresh_token  = refresh_token
+        
+        self.client = fitbit.Fitbit(
+            self.consumer_key,
+            self.consumer_secret,
+            access_token=self.access_token,
+            refresh_token=self.refresh_token, 
+            refresh_cb=self.update_tokens)
+        self.goal = self.get_goal()
+
+    def update_tokens(self, token):
+        """
+        Callback to update token
+        """
+        config = configparser.ConfigParser()
+        config.read(CONFIG_FILE)
+        config.set(self.player, "REFRESH_TOKEN", token['refresh_token'])
+        config.set(self.player, "ACCESS_TOKEN", token['access_token'])
+
+        with open(CONFIG_FILE, "w") as config_file:
+            config.write(config_file)
+
+    def get_steps(self):
+        """
+            Return the steps logged
+        """
+        num_steps = 0
+        now = datetime.datetime.now()
+        end_time = now.strftime("%H:%M")
+        try:
+            response = self.client.intraday_time_series(
+                'activities/steps',
+                detail_level='15min',
+                start_time="00:00",
+                end_time=end_time
+            )
+        except Exception as error:
+            print(error)
+        else:
+            str_steps = response['activities-steps'][0]['value']
+            print(str_steps)
+            try:
+                num_steps = int(str_steps)
+            except ValueError:
+                pass
+        return num_steps
+
+    def get_goal(self):
+        """
+            Determine Daily step goal
+        """
+        num_steps = 0
+
+        try:
+            response = self.client.activities_daily_goal()
+        except Exception as error:
+            print(error)
+        
+        return response['goals']['steps']
 
 if __name__ == "__main__":
-    fbit_client = fitbit.Fitbit(CONSUMER_KEY,
-                                CONSUMER_SECRET,
-                                access_token=ACCESS_TOKEN,
-                                refresh_token=REFRESH_TOKEN)
-    now = datetime.datetime.now()
-    end_time = now.strftime("%H:%M")
-    response = fbit_client.intraday_time_series('activities/steps',
-                                                detail_level='15min',
-                                                start_time="00:00",
-                                                end_time=end_time)
-    print(response['activities-steps'][0]['value'])
+    scoreboard = []
+    players = []
+    
+    # config is loaded from config file
+    # alternatively you may store them as constants in your program
+    CONFIG_FILE = '/home/pi/score_board.ini'
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE)
 
-    response = fbit_client.activities_daily_goal()
-    print(response['goals']['steps'])
+    for index in range(NUM_PLAYERS):
+        players[index] = "NUMBER_" + str(index + 1) 
+    
+    for index in range(NUM_PLAYERS):
+        consumer_key = config.get(player[index], "CONSUMER_KEY")
+        consumer_secret = config.get(player[index], "CONSUMER_SECRET")
+        refresh_token = config.get(player[index], "REFRESH_TOKEN")
+        access_token = config.get(player[index], "ACCESS_TOKEN")
+        scoreboard[index] = VisualAid(
+            player[index],
+            consumer_key,
+            consumer_token,
+            access_token=access_token,
+            refresh_token=refresh_token
+        )
+
+        print(scoreboard[index]).get_steps()
+
+
